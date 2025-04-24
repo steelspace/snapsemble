142476201090f90ee972310e138d9687523f6bfe
Petr Valach <petr.valach@hotmail.com>
2025-04-24 10:40:52 +0200

    a

--- lazy-load.js
+++ lazy-load.js
@@ -1,15 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
    const lazyImages = document.querySelectorAll(".lazy-load");
 
+   const imageObserver = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+         if (entry.isIntersecting) {
+            const img = entry.target;
+            const lowResSrc = img.getAttribute("src");
+            const highResSrc = lowResSrc.replace(".webp", ".png");
+
+            const highResImage = new Image();
+            highResImage.src = highResSrc;
+
+            highResImage.onload = () => {
+               img.src = highResImage.src;
+            };
+
+            observer.unobserve(img); // Stop observing once the image is loaded
+         }
+      });
+   });
+
    lazyImages.forEach((img) => {
-      const lowResSrc = img.getAttribute("src");
-      const highResSrc = lowResSrc.replace(".webp", ".png");
-
-      const highResImage = new Image();
-      highResImage.src = highResSrc;
-
-      highResImage.onload = () => {
-         img.src = highResImage.src;
-      };
+      imageObserver.observe(img);
    });
 });
